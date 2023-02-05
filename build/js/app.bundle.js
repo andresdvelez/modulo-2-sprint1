b78081f7dda74a2d01bc4615645a80d6deaf4ccc
Andres David Velez Suaza <avelez0824@gmail.com>
2023-02-05 15:56:16 -0500

    built

--- build/js/app.bundle.js
+++ build/js/app.bundle.js
@@ -26,7 +26,37 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _sty
   \***************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _images_profileImg_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../images/profileImg.png */ \"./src/images/profileImg.png\");\n// Setting images\nvar chatsProfileImg = document.getElementById(\"chatsProfileImg\");\nvar searchIcon = document.getElementById(\"searchIcon\");\nvar chatsImg = document.getElementById(\"chatsImg\");\nvar readIcon = document.getElementById(\"readIcon\");\nvar msgProfileImg = document.getElementById(\"msgProfileImg\");\nvar emojisBtn = document.getElementById(\"emojisBtn\");\nvar filesBtn = document.getElementById(\"filesBtn\");\nvar msgSendBtn = document.getElementById(\"msgSendBtn\");\n\nchatsProfileImg.src = _images_profileImg_png__WEBPACK_IMPORTED_MODULE_0__[\"default\"];\n\n//# sourceURL=webpack://sprint-1/./src/scripts/UI.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _images_chat_bg_jpeg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../images/chat-bg.jpeg */ \"./src/images/chat-bg.jpeg\");\n/* harmony import */ var _images_profileImg_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../images/profileImg.png */ \"./src/images/profileImg.png\");\n/* harmony import */ var _images_search_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../images/search.svg */ \"./src/images/search.svg\");\n/* harmony import */ var _images_check_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../images/check.svg */ \"./src/images/check.svg\");\n/* harmony import */ var _images_smile_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../images/smile.svg */ \"./src/images/smile.svg\");\n/* harmony import */ var _images_paperclip_svg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../images/paperclip.svg */ \"./src/images/paperclip.svg\");\n/* harmony import */ var _images_send_svg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../images/send.svg */ \"./src/images/send.svg\");\n// Setting images\nvar chatsProfileImg = document.getElementById(\"chatsProfileImg\");\nvar searchIcon = document.querySelectorAll(\"#searchIcon\");\nvar chatsImg = document.getElementById(\"chatsImg\");\nvar readIcon = document.querySelectorAll(\"#readIcon\");\nvar msgProfileImg = document.getElementById(\"msgProfileImg\");\nvar emojisBtn = document.getElementById(\"emojisBtn\");\nvar filesBtn = document.getElementById(\"filesBtn\");\nvar msgSendBtn = document.getElementById(\"msgSendBtn\");\nvar chatBg = document.getElementById(\"chatBg\");\n\n\n\n\n\n\n\nchatBg.src = _images_chat_bg_jpeg__WEBPACK_IMPORTED_MODULE_0__[\"default\"];\nmsgSendBtn.src = _images_send_svg__WEBPACK_IMPORTED_MODULE_6__[\"default\"];\nfilesBtn.src = _images_paperclip_svg__WEBPACK_IMPORTED_MODULE_5__[\"default\"];\nemojisBtn.src = _images_smile_svg__WEBPACK_IMPORTED_MODULE_4__[\"default\"];\nmsgProfileImg.src = _images_profileImg_png__WEBPACK_IMPORTED_MODULE_1__[\"default\"];\nchatsImg.src = _images_profileImg_png__WEBPACK_IMPORTED_MODULE_1__[\"default\"];\nchatsProfileImg.src = _images_profileImg_png__WEBPACK_IMPORTED_MODULE_1__[\"default\"];\nfor (var i = 0, len = searchIcon.length; i < len; i++) {\n  searchIcon[i].src = _images_search_svg__WEBPACK_IMPORTED_MODULE_2__[\"default\"];\n}\nfor (var _i = 0, _len = readIcon.length; _i < _len; _i++) {\n  readIcon[_i].src = _images_check_svg__WEBPACK_IMPORTED_MODULE_3__[\"default\"];\n}\n\n//# sourceURL=webpack://sprint-1/./src/scripts/UI.js?");
+
+/***/ }),
+
+/***/ "./src/images/chat-bg.jpeg":
+/*!*********************************!*\
+  !*** ./src/images/chat-bg.jpeg ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/chat-bg.jpeg\");\n\n//# sourceURL=webpack://sprint-1/./src/images/chat-bg.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/check.svg":
+/*!******************************!*\
+  !*** ./src/images/check.svg ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/check.svg\");\n\n//# sourceURL=webpack://sprint-1/./src/images/check.svg?");
+
+/***/ }),
+
+/***/ "./src/images/paperclip.svg":
+/*!**********************************!*\
+  !*** ./src/images/paperclip.svg ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/paperclip.svg\");\n\n//# sourceURL=webpack://sprint-1/./src/images/paperclip.svg?");
 
 /***/ }),
 
@@ -37,6 +67,36 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _ima
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/profileImg.png\");\n\n//# sourceURL=webpack://sprint-1/./src/images/profileImg.png?");
+
+/***/ }),
+
+/***/ "./src/images/search.svg":
+/*!*******************************!*\
+  !*** ./src/images/search.svg ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/search.svg\");\n\n//# sourceURL=webpack://sprint-1/./src/images/search.svg?");
+
+/***/ }),
+
+/***/ "./src/images/send.svg":
+/*!*****************************!*\
+  !*** ./src/images/send.svg ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/send.svg\");\n\n//# sourceURL=webpack://sprint-1/./src/images/send.svg?");
+
+/***/ }),
+
+/***/ "./src/images/smile.svg":
+/*!******************************!*\
+  !*** ./src/images/smile.svg ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"images/smile.svg\");\n\n//# sourceURL=webpack://sprint-1/./src/images/smile.svg?");
 
 /***/ }),
 
